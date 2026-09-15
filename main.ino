8a6d6e1489fcf8dda22d0c9adf263cd1a6a26793
Eher1r <kadennotabot3@gmail.com>
2026-09-16 05:55:21 +0800

    2 buttons done with starting loading bar

--- main.ino
+++ main.ino
@@ -2,12 +2,16 @@
 #include <Adafruit_SSD1306.h>
 #include <Arduino.h>
 #include <DHT.h>  // DHT sensor library
+#include <OneButton.h>
 #include <Wire.h>
 
+// Prototypes
 int Gen_snpp(int type, char* destination, int size, float reading);
 void printCentered(char* text, int y, unsigned int size);
 int update_Toggle(int num);
 void display012(void);
+void startDeepSleep(void);
+void startAnimation(const char* label, int duration);
 
 // DHT22 Sensor
 #define DHTPIN 4  // Pin on ESP connected to DATA pin on DHT22
@@ -22,10 +26,13 @@ void display012(void);
 #define SCL_PIN 9
 
 // Buttons
-#define BUTTON_PIN 7
+#define BUTTON_PIN1 7
+#define BUTTON_PIN2 2
 
 DHT dht(DHTPIN, DHTTYPE);  // Object for DHT22
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);  // Object for OLED
+// Sleep objects
+OneButton button(BUTTON_PIN2, true);
 
 // Global variables
 char resultH[32];
@@ -39,7 +46,7 @@ int button_Toggle = 0;
 
 void setup()
 {
-  // Start stuff up
+  // Secure Serial connection
   Serial.begin(115200);
   delay(1000);
 
@@ -56,27 +63,33 @@ void setup()
   }
 
   Serial.println("--- ESP32-C3 DHT22 Project Initialization ---");
+  startAnimation("LOADING...", 2500);
 
   // Start stuff up
   dht.begin();
   Wire.begin(SDA_PIN, SCL_PIN);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_PIN1, INPUT_PULLUP);
+  pinMode(BUTTON_PIN2, INPUT_PULLUP);
 
   // Settings
   display.setTextColor(SSD1306_WHITE);
-
+  button.setPressMs(3000);
+  button.attachLongPressStart(startDeepSleep);
+  
   delay(1500); // You Ready?
 }
 
 
 // Measure running time for counting when the DHT22 would take the reading
 const unsigned long run = millis();
-const unsigned long reading_pause = 5000;
+const unsigned long reading_pause = 3000;
 unsigned long current_time;
 unsigned long last_time = 0;
 
 void loop()
 {
+  button.tick();
+
   // Check if it is time to take the reading
   unsigned long current_time = millis() - run;
   error = false; // For error checking
@@ -112,8 +125,8 @@ void loop()
     display012();
   }
 
-  // Button code
-  int reading = digitalRead(BUTTON_PIN);
+  // Button1 code
+  int reading = digitalRead(BUTTON_PIN1);
   int c = 0;
   if (reading != last_Button_State)
   {
@@ -206,4 +219,60 @@ void display012(void)
   }
   display.display();
   Serial.printf("Displayed\n");
+}
+
+void startDeepSleep(void)
+{
+  // Trun off OLED
+  display.clearDisplay();
+  display.display();
+  display.ssd1306_command(SSD1306_DISPLAYOFF);
+
+  while (digitalRead(BUTTON_PIN2) == LOW)
+  {
+    delay(50);
+  }
+  delay(1000);
+
+  esp_deep_sleep_enable_gpio_wakeup(1ULL << BUTTON_PIN2, ESP_GPIO_WAKEUP_GPIO_LOW);
+  esp_deep_sleep_start();
+}
+
+void startAnimation(const char* label, int duration)
+{
+  // Dimensions for the loading bar outline
+  int barWidth = 100;
+  int barHeight = 12;
+  int barX = (SCREEN_WIDTH - barWidth) / 2;
+  int barY = 36;
+  
+  int padding = 2; // Space between outer border and inner fill
+  int maxFillWidth = barWidth - (padding * 2);
+  int steps = 50; // Smoothness of the fill
+  int delayPerStep = duration / steps;
+
+  for (int i = 0; i <= steps; i++) {
+    display.clearDisplay();
+    display.setTextSize(1);
+    
+    // Center the header text dynamically
+    int16_t x1, y1;
+    uint16_t w, h;
+    display.getTextBounds(label, 0, 0, &x1, &y1, &w, &h);
+    display.setCursor((SCREEN_WIDTH - w) / 2, 16);
+    display.print(label);
+
+    // 2. Draw Outer Rounded Frame
+    display.drawRoundRect(barX, barY, barWidth, barHeight, 3, SSD1306_WHITE);
+
+    // 3. Calculate and Draw Inner Progress Fill
+    int currentFillWidth = map(i, 0, steps, 0, maxFillWidth);
+    if (currentFillWidth > 0) {
+      display.fillRoundRect(barX + padding, barY + padding, currentFillWidth, barHeight - (padding * 2), 2, SSD1306_WHITE);
+    }
+
+    display.display();
+    delay(delayPerStep);
+  }
+  delay(200); // Brief pause
 }
