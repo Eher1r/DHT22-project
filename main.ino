b9bc090dc0f02de891d3dcbf925fc072d70f0485
Eher1r <kadennotabot3@gmail.com>
2026-09-17 16:06:38 +0800

    Try 2 screens

--- main.ino
+++ main.ino
@@ -22,7 +22,10 @@ void shutdownAnimation();
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
+
 #define SCREEN_ADDRESS 0x3C
+#define SDA_PINH x
+#define SCL_PINH y
 #define SDA_PIN 8
 #define SCL_PIN 9
 
@@ -35,7 +38,7 @@ void shutdownAnimation();
 
 DHT dht(DHTPIN, DHTTYPE);  // Object for DHT22
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);  // Object for OLED
-// Sleep objects
+Adafruit_SSD1306 displayHome(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 OneButton button(BUTTON_PIN2_1, true);
 
 // Global variables
@@ -43,6 +46,9 @@ char resultH[32];
 char resultT[32];
 const unsigned long click_Delay = 50;
 bool error;
+bool readStart = false;
+int homescreentog = 0;
+bool oled2on = false;
 
 int button_State11 = HIGH;
 int last_Button_State11 = HIGH;
@@ -73,9 +79,15 @@ void setup()
     delay(100);
   }
   
+  if (!displayHome.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+  {
+    Serial.println("Failed to connect with OLED display1 :()");
+    for (;;);
+  }
+
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
   {
-    Serial.println("Failed to connect with OLED display :()");
+    Serial.println("Failed to connect with OLED display2 :()");
     for (;;);
   }
 
@@ -93,9 +105,14 @@ void setup()
 
   // Settings
   display.setTextColor(SSD1306_WHITE);
+  displayHome.setTextSize(SSD1306_WHITE);
   button.setPressMs(3000);
   button.attachLongPressStart(startDeepSleep);
   
+  display.clearDisplay();
+  display.display();
+  displayHomScreen();
+
   delay(1500); // You Ready?
 }
 
@@ -133,16 +150,19 @@ void loop()
       return;
     }
 
-    lengthH = Gen_snpp(0, resultH, sizeof(resultH), humidity);
-    lengthT = Gen_snpp(1, resultT, sizeof(resultT), temp);
-    
-    // Error checking
-    if (lengthH <= 0 || lengthT <= 0 || lengthH > 168 || lengthT > 168)
+    if (oled2on)
     {
-      error = true;
-    }
+      lengthH = Gen_snpp(0, resultH, sizeof(resultH), humidity);
+      lengthT = Gen_snpp(1, resultT, sizeof(resultT), temp);
 
-    display012();
+      // Error checking
+      if (lengthH <= 0 || lengthT <= 0 || lengthH > 168 || lengthT > 168)
+      {
+        error = true;
+      }
+
+      displayReading();
+    }
   }
 
   // Button11 code
@@ -157,13 +177,37 @@ void loop()
       button_State11 = reading11;
       if (button_State11 == LOW)
       {
-        ...
+        if (screentog == 0)
+        {
+          screentog = 1;
+        }
+        else if (screentog == 1)
+        {
+          screentog -= 1;
+        }
       }
     }
   }
 
   // Button12 code
   int reading12 = digitalRead(BUTTON_PIN12);
+  if (reading12 != last_Button_State12)
+    last_Click_Time12 = millis();
+
+  if ((millis() - last_Click_Time12) > click_Delay)
+  {
+    if (reading12 != button_State12)
+    {
+      button_State12 = reading12;
+      if (button_State12 == LOW)
+      {
+        ...
+      }
+    }
+  }
+
+// Button13 code
+int reading12 = digitalRead(BUTTON_PIN12);
   if (reading12 != last_Button_State12)
     last_Click_Time12 = millis();
 
@@ -192,7 +236,7 @@ void loop()
       if (button_State22 == LOW)
       {
         button_Toggle = update_Toggle(button_Toggle);
-        display012();
+        displayReading();
       }
     }
   }
@@ -220,7 +264,7 @@ int Gen_snpp(int type, char* destination, int size, float reading)  // snprintf 
   return -1;  // Both conditions above failed to run
 }
 
-void printCentered(char* text, int y, unsigned int size)  // Centers the text and prints it out
+void printCentered(char* text, int y, unsigned int size, int screen)  // Centers the text and prints it out
 {
   // Variables used in .getTextBounds
   int16_t x1;
@@ -228,12 +272,24 @@ void printCentered(char* text, int y, unsigned int size)  // Centers the text an
   uint16_t width;
   uint16_t height;
 
-  display.setTextSize(size);
-  display.getTextBounds(text, 0, 0, &x1, &y1, &width, &height);
+  if (screen == 1)
+  {
+    displayH.setTextSize(size);
+    displayH.getTextBounds(text, 0, 0, &x1, &y1, &width, &height);
 
-  int x = (SCREEN_WIDTH - width) / 2;  // Calculate the start point which allow text to be centered
-  display.setCursor(x, y);
-  display.print(text);
+    int x = (SCREEN_WIDTH - width) / 2;  // Calculate the start point which allow text to be centered
+    displayH.setCursor(x, y);
+    displayH.print(text);
+  }
+  else if (screen == 2)
+  {
+    display.setTextSize(size);
+    display.getTextBounds(text, 0, 0, &x1, &y1, &width, &height);
+
+    int x = (SCREEN_WIDTH - width) / 2;  // Calculate the start point which allow text to be centered
+    display.setCursor(x, y);
+    display.print(text);
+  }
 }
 
 int update_Toggle(int num)
@@ -252,21 +308,54 @@ int update_Toggle(int num)
   }
 }
 
-void display012(void)
+void displayHomeScreen(void)
+{
+  displayH.clearDisplay();
+  printCentered("Home", x, 2, 1);
+  if (homescreentog == 0)
+  {
+    printCentered("[ Start DHT22 ]", x, 1);
+    printCentered("Save Data", x, 1);
+  }
+  else if (homesreentog == 1)
+  {
+    printCentered("Start DHT22", x, 1);
+    printCentered("[ Save Data ]", x, 1);
+  }
+  else if (homescreentog == 11)
+  {
+    printCentered("[ Stop DHT22 ]", x, 1);
+    printCentered("Save Data", x, 1);
+  }
+  else if (homesreentog == 12)
+  {
+    printCentered("Stop DHT22", x, 1);
+    printCentered("[ Save Data ]", x, 1);
+  }
+  else
+  {
+    error = true;
+    return;
+  }
+  
+  displayH.display();
+}
+
+void displayReading(void)
 {
   display.clearDisplay();
   if (button_Toggle == 0)  // Both Humidity and Temperature
   {
-    printCentered(resultH, 22, 1);
-    printCentered(resultT, 34, 1);
+    printCentered(resultH, 22, 1, 2);
+    printCentered(resultT, 34, 1, 2);
   }
   else if (button_Toggle == 1)  // Just Temperature
   {
-    printCentered(resultT, 28, 1);
+    printCentered(resultT, 28, 1, 2);
   }
   else if (button_Toggle == 2)  // Just Humidity
   {
-    printCentered(resultH, 28, 1);
+    printCentered(resultH, 28, 1, 2);
   }
   else
   {
@@ -279,9 +368,12 @@ void startDeepSleep(void)
 {
   // Trun off OLED
   display.clearDisplay();
+  displayHome.clearDisplay();
   shutdownAnimation();
   display.display();
+  displayHome.display();
   display.ssd1306_command(SSD1306_DISPLAYOFF);
+  displayHome.ssd1306_command(SSD1306_DISPLAYOFF);
 
   while (digitalRead(BUTTON_PIN2) == LOW)
   {
@@ -309,6 +401,9 @@ void startAnimation(const char* label, int duration)
   for (int i = 0; i <= steps; i++) {
     display.clearDisplay();
     display.setTextSize(1);
+
+    displayHome.clearDisplay();
+    displayHome.setTextSize(1);
     
     // Center the header text dynamically
     int16_t x1, y1;
@@ -317,16 +412,25 @@ void startAnimation(const char* label, int duration)
     display.setCursor((SCREEN_WIDTH - w) / 2, 16);
     display.print(label);
 
+    int16_t x2, y2;
+    uint16_t wid, hei;
+    displayHome.getTextBounds(label, 0, 0, &x2, &y2, &wid, &hei);
+    displayHome.setCursor((SCREEN_WIDTH - wid) / 2, 16);
+    displayHome.print(label);
+
     // 2. Draw Outer Rounded Frame
     display.drawRoundRect(barX, barY, barWidth, barHeight, 3, SSD1306_WHITE);
+    displayHome.drawRoundRect(barX, barY, barWidth, barHeight, 3, SSD1306_WHITE);
 
     // 3. Calculate and Draw Inner Progress Fill
     int currentFillWidth = map(i, 0, steps, 0, maxFillWidth);
     if (currentFillWidth > 0) {
       display.fillRoundRect(barX + padding, barY + padding, currentFillWidth, barHeight - (padding * 2), 2, SSD1306_WHITE);
+      displayHome.fillRoundRect(barX + padding, barY + padding, currentFillWidth, barHeight - (padding * 2), 2, SSD1306_WHITE);
     }
 
     display.display();
+    displayHome.display();
     delay(delayPerStep);
   }
   delay(200);
@@ -340,22 +444,31 @@ void shutdownAnimation()
   // Screen squashes down into horizontal line
   for (int h = SCREEN_HEIGHT / 2; h > 1; h -= 2) {
     display.clearDisplay();
+    displayHome.clearDisplay();
     display.fillRect(0, centerY - h, SCREEN_WIDTH, h * 2, SSD1306_WHITE);
+    displayHome.fillRect(0, centerY - h, SCREEN_WIDTH, h * 2, SSD1306_WHITE);
     display.display();
+    displayHome.display()
     delay(15);
   }
 
   // Line shrinks into dot)
   for (int w = SCREEN_WIDTH / 2; w > 2; w -= 4) {
     display.clearDisplay();
+    displayHome.clearDisplay();
     display.fillRect(centerX - w, centerY - 1, w * 2, 2, SSD1306_WHITE);
+    displayHome.fillRect(centerX - w, centerY - 1, w * 2, 2, SSD1306_WHITE);
     display.display();
+    displayHome.display();
     delay(10);
   }
 
   // Flash and fade dot
   display.clearDisplay();
+  displayHome.clearDisplay();
   display.fillCircle(centerX, centerY, 2, SSD1306_WHITE);
+  displayHome.fillCircle(centerX, centerY, 2, SSD1306_WHITE);
   display.display();
+  displayHome.display();
   delay(100);
 }
